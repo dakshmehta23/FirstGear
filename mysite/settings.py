946894bf2e6ceb416d11a47da80028c4740c2fc7
dakshmehta23 <45225674+dakshmehta23@users.noreply.github.com>
2021-11-23 16:53:32 +0530

    static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# new for deploying
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 #Managing Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
